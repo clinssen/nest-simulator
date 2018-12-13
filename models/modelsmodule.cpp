f4f613fb6155d946f09e9d88155b28dc12d181d9
C.A.P. Linssen <charl@turingbirds.com>
2018-12-13 15:47:41 +0100

    templated version of register_connection_model() helper function

--- models/modelsmodule.cpp
+++ models/modelsmodule.cpp
@@ -36,129 +36,6 @@
 
 #include "modelsmodule.h"
 
-// Includes from nestkernel
-#include "genericmodel_impl.h"
-
-// Generated includes:
-#include "config.h"
-
-// Neuron models
-#include "aeif_cond_alpha.h"
-#include "aeif_cond_alpha_RK5.h"
-#include "aeif_cond_alpha_multisynapse.h"
-#include "aeif_cond_beta_multisynapse.h"
-#include "aeif_cond_exp.h"
-#include "aeif_psc_alpha.h"
-#include "aeif_psc_delta.h"
-#include "aeif_psc_exp.h"
-#include "amat2_psc_exp.h"
-#include "erfc_neuron.h"
-#include "gauss_rate.h"
-#include "gif_cond_exp.h"
-#include "gif_cond_exp_multisynapse.h"
-#include "gif_pop_psc_exp.h"
-#include "gif_psc_exp.h"
-#include "gif_psc_exp_multisynapse.h"
-#include "ginzburg_neuron.h"
-#include "hh_cond_exp_traub.h"
-#include "hh_psc_alpha.h"
-#include "hh_psc_alpha_gap.h"
-#include "ht_neuron.h"
-#include "iaf_chs_2007.h"
-#include "iaf_chxk_2008.h"
-#include "iaf_cond_alpha.h"
-#include "iaf_cond_alpha_mc.h"
-#include "iaf_cond_exp.h"
-#include "iaf_cond_exp_sfa_rr.h"
-#include "iaf_psc_alpha.h"
-#include "iaf_psc_alpha_multisynapse.h"
-#include "iaf_psc_delta.h"
-#include "iaf_psc_exp.h"
-#include "iaf_psc_exp_multisynapse.h"
-#include "iaf_tum_2000.h"
-#include "izhikevich.h"
-#include "lin_rate.h"
-#include "mat2_psc_exp.h"
-#include "mcculloch_pitts_neuron.h"
-#include "parrot_neuron.h"
-#include "pp_pop_psc_delta.h"
-#include "pp_psc_delta.h"
-#include "siegert_neuron.h"
-#include "sigmoid_rate.h"
-#include "sigmoid_rate_gg_1998.h"
-#include "tanh_rate.h"
-#include "threshold_lin_rate.h"
-
-// Stimulation devices
-#include "ac_generator.h"
-#include "dc_generator.h"
-#include "gamma_sup_generator.h"
-#include "inhomogeneous_poisson_generator.h"
-#include "mip_generator.h"
-#include "noise_generator.h"
-#include "poisson_generator.h"
-#include "ppd_sup_generator.h"
-#include "pulsepacket_generator.h"
-#include "sinusoidal_gamma_generator.h"
-#include "sinusoidal_poisson_generator.h"
-#include "spike_generator.h"
-#include "step_current_generator.h"
-
-// Recording devices
-#include "correlation_detector.h"
-#include "correlomatrix_detector.h"
-#include "correlospinmatrix_detector.h"
-#include "multimeter.h"
-#include "voltmeter.h"
-#include "spike_detector.h"
-#include "spin_detector.h"
-#include "weight_recorder.h"
-
-#include "volume_transmitter.h"
-
-// Prototypes for synapses
-#include "bernoulli_connection.h"
-#include "common_synapse_properties.h"
-#include "cont_delay_connection.h"
-#include "cont_delay_connection_impl.h"
-#include "diffusion_connection.h"
-#include "gap_junction.h"
-#include "ht_connection.h"
-#include "quantal_stp_connection.h"
-#include "quantal_stp_connection_impl.h"
-#include "rate_connection_delayed.h"
-#include "rate_connection_instantaneous.h"
-#include "spike_dilutor.h"
-#include "static_connection.h"
-#include "static_connection_hom_w.h"
-#include "stdp_connection.h"
-#include "stdp_connection_facetshw_hom.h"
-#include "stdp_connection_facetshw_hom_impl.h"
-#include "stdp_connection_hom.h"
-#include "stdp_dopa_connection.h"
-#include "stdp_pl_connection_hom.h"
-#include "stdp_triplet_connection.h"
-#include "tsodyks2_connection.h"
-#include "tsodyks_connection.h"
-#include "tsodyks_connection_hom.h"
-#include "vogels_sprekeler_connection.h"
-
-// Includes from nestkernel:
-#include "common_synapse_properties.h"
-#include "connector_model_impl.h"
-#include "genericmodel.h"
-#include "kernel_manager.h"
-#include "model.h"
-#include "model_manager_impl.h"
-#include "target_identifier.h"
-
-#ifdef HAVE_MUSIC
-#include "music_cont_in_proxy.h"
-#include "music_cont_out_proxy.h"
-#include "music_event_in_proxy.h"
-#include "music_event_out_proxy.h"
-#include "music_message_in_proxy.h"
-#endif
 
 namespace nest
 {
@@ -346,7 +223,7 @@ ModelsModule::init( SLIInterpreter* )
     /*deprecation_info*/ "NEST 3.0" );
 
 #ifdef HAVE_MUSIC
-  //// proxies for inter-application communication using MUSIC
+  // proxies for inter-application communication using MUSIC
   kernel().model_manager.register_node_model< music_event_in_proxy >(
     "music_event_in_proxy" );
   kernel().model_manager.register_node_model< music_event_out_proxy >(
@@ -359,13 +236,23 @@ ModelsModule::init( SLIInterpreter* )
     "music_message_in_proxy" );
 #endif
 
-  // register all connection models: 
-  // once for the normal indexing type
-  // once for the ConnectionLabel type (postfix: "_lbl")
-  // once for the high-performance indexing type (postfix: "_hpc")
-  register_connection_models< TargetIdentifierPtrRport >();
-  register_connection_models< ConnectionLabel< TargetIdentifierPtrRport > >( "_lbl" );
-  register_connection_models< TargetIdentifierIndex >( "_hpc" );
+  // register all connection models
+  register_connection_model< StaticConnection >(std::string("static_synapse"));
+/*  register_connection_model< StaticConnectionHomW >("static_synapse_hom_w");
+  register_connection_model< STDPConnection >("stdp_synapse");
+  register_connection_model< STDPPLConnectionHom("stdp_pl_synapse_hom");
+  register_connection_model< STDPTripletConnection >("stdp_triplet_synapse");
+  register_connection_model< Quantal_StpConnection >("quantal_stp_synapse");
+  register_connection_model< STDPConnectionHom >("stdp_synapse_hom");
+  register_connection_model< STDPFACETSHWConnectionHom >("stdp_facetshw_synapse_hom");
+  register_connection_model< ContDelayConnection>("cont_delay_synapse");
+  register_connection_model< TsodyksConnection >("tsodyks_synapse");
+  register_connection_model< TsodyksConnectionHom >("tsodyks_synapse_hom");
+  register_connection_model< Tsodyks2Connection >("tsodyks2_synapse");
+  register_connection_model< HTConnection >("ht_synapse");
+  register_connection_model< STDPDopaConnection >("stdp_dopamine_synapse");
+  register_connection_model< VogelsSprekelerConnection >("vogels_sprekeler_synapse");
+  register_connection_model< BernoulliConnection>("bernoulli_synapse");*/
 
   // register secondary connection models
   kernel()
@@ -398,71 +285,20 @@ ModelsModule::init( SLIInterpreter* )
       /*supports_wfr=*/true );
 }
 
-template < typename ConnectionT >
+
+/*template < typename ConnectionClassT >
 void
-ModelsModule::register_connection_models( std::string name_postfix )
+ModelsModule::register_connection_model( const std::string name )
 {
   kernel()
-    .model_manager.register_connection_model< StaticConnection< ConnectionT > >(
-      "static_synapse" + name_postfix );
+    .model_manager.register_connection_model< ConnectionClassT < TargetIdentifierPtrRport > >(
+      name );
   kernel()
-    .model_manager
-    .register_connection_model< StaticConnectionHomW< ConnectionT > >(
-      "static_synapse_hom_w" + name_postfix );
+    .model_manager.register_connection_model< ConnectionLabel < ConnectionClassT < TargetIdentifierPtrRport > > >(
+      name + "_lbl" );
   kernel()
-    .model_manager.register_connection_model< STDPConnection< ConnectionT > >(
-      "stdp_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< STDPPLConnectionHom< ConnectionT > >(
-      "stdp_pl_synapse_hom" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< STDPTripletConnection< ConnectionT > >(
-      "stdp_triplet_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< Quantal_StpConnection< ConnectionT > >(
-      "quantal_stp_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< STDPConnectionHom< ConnectionT > >(
-      "stdp_synapse_hom" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< STDPFACETSHWConnectionHom< ConnectionT > >(
-      "stdp_facetshw_synapse_hom" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< ContDelayConnection< ConnectionT > >(
-      "cont_delay_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< TsodyksConnection< ConnectionT > >(
-      "tsodyks_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< TsodyksConnectionHom< ConnectionT > >(
-      "tsodyks_synapse_hom" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< Tsodyks2Connection< ConnectionT > >(
-      "tsodyks2_synapse" + name_postfix );
-  kernel()
-    .model_manager.register_connection_model< HTConnection< ConnectionT > >(
-      "ht_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< STDPDopaConnection< ConnectionT > >(
-      "stdp_dopamine_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< VogelsSprekelerConnection< ConnectionT > >(
-      "vogels_sprekeler_synapse" + name_postfix );
-  kernel()
-    .model_manager
-    .register_connection_model< BernoulliConnection< ConnectionT > >(
-      "bernoulli_synapse" + name_postfix );
-}
+    .model_manager.register_connection_model< ConnectionClassT < TargetIdentifierIndex > >(
+      name + "_hpc" );
+}*/<
 
 } // namespace nest
--- models/modelsmodule.h
+++ models/modelsmodule.h
@@ -31,6 +31,131 @@
 #include "genericmodel.h"
 #include "slimodule.h"
 
+// Includes from nestkernel
+#include "genericmodel_impl.h"
+
+// Generated includes:
+#include "config.h"
+
+// Neuron models
+#include "aeif_cond_alpha.h"
+#include "aeif_cond_alpha_RK5.h"
+#include "aeif_cond_alpha_multisynapse.h"
+#include "aeif_cond_beta_multisynapse.h"
+#include "aeif_cond_exp.h"
+#include "aeif_psc_alpha.h"
+#include "aeif_psc_delta.h"
+#include "aeif_psc_exp.h"
+#include "amat2_psc_exp.h"
+#include "erfc_neuron.h"
+#include "gauss_rate.h"
+#include "gif_cond_exp.h"
+#include "gif_cond_exp_multisynapse.h"
+#include "gif_pop_psc_exp.h"
+#include "gif_psc_exp.h"
+#include "gif_psc_exp_multisynapse.h"
+#include "ginzburg_neuron.h"
+#include "hh_cond_exp_traub.h"
+#include "hh_psc_alpha.h"
+#include "hh_psc_alpha_gap.h"
+#include "ht_neuron.h"
+#include "iaf_chs_2007.h"
+#include "iaf_chxk_2008.h"
+#include "iaf_cond_alpha.h"
+#include "iaf_cond_alpha_mc.h"
+#include "iaf_cond_exp.h"
+#include "iaf_cond_exp_sfa_rr.h"
+#include "iaf_psc_alpha.h"
+#include "iaf_psc_alpha_multisynapse.h"
+#include "iaf_psc_delta.h"
+#include "iaf_psc_exp.h"
+#include "iaf_psc_exp_multisynapse.h"
+#include "iaf_tum_2000.h"
+#include "izhikevich.h"
+#include "lin_rate.h"
+#include "mat2_psc_exp.h"
+#include "mcculloch_pitts_neuron.h"
+#include "parrot_neuron.h"
+#include "pp_pop_psc_delta.h"
+#include "pp_psc_delta.h"
+#include "siegert_neuron.h"
+#include "sigmoid_rate.h"
+#include "sigmoid_rate_gg_1998.h"
+#include "tanh_rate.h"
+#include "threshold_lin_rate.h"
+
+// Stimulation devices
+#include "ac_generator.h"
+#include "dc_generator.h"
+#include "gamma_sup_generator.h"
+#include "inhomogeneous_poisson_generator.h"
+#include "mip_generator.h"
+#include "noise_generator.h"
+#include "poisson_generator.h"
+#include "ppd_sup_generator.h"
+#include "pulsepacket_generator.h"
+#include "sinusoidal_gamma_generator.h"
+#include "sinusoidal_poisson_generator.h"
+#include "spike_generator.h"
+#include "step_current_generator.h"
+
+// Recording devices
+#include "correlation_detector.h"
+#include "correlomatrix_detector.h"
+#include "correlospinmatrix_detector.h"
+#include "multimeter.h"
+#include "voltmeter.h"
+#include "spike_detector.h"
+#include "spin_detector.h"
+#include "weight_recorder.h"
+
+#include "volume_transmitter.h"
+
+// Prototypes for synapses
+#include "bernoulli_connection.h"
+#include "common_synapse_properties.h"
+#include "cont_delay_connection.h"
+#include "cont_delay_connection_impl.h"
+#include "diffusion_connection.h"
+#include "gap_junction.h"
+#include "ht_connection.h"
+#include "quantal_stp_connection.h"
+#include "quantal_stp_connection_impl.h"
+#include "rate_connection_delayed.h"
+#include "rate_connection_instantaneous.h"
+#include "spike_dilutor.h"
+#include "static_connection.h"
+#include "static_connection_hom_w.h"
+#include "stdp_connection.h"
+#include "stdp_connection_facetshw_hom.h"
+#include "stdp_connection_facetshw_hom_impl.h"
+#include "stdp_connection_hom.h"
+#include "stdp_dopa_connection.h"
+#include "stdp_pl_connection_hom.h"
+#include "stdp_triplet_connection.h"
+#include "tsodyks2_connection.h"
+#include "tsodyks_connection.h"
+#include "tsodyks_connection_hom.h"
+#include "vogels_sprekeler_connection.h"
+
+// Includes from nestkernel:
+#include "common_synapse_properties.h"
+#include "connector_model_impl.h"
+#include "genericmodel.h"
+#include "kernel_manager.h"
+#include "model.h"
+#include "model_manager_impl.h"
+#include "target_identifier.h"
+
+#ifdef HAVE_MUSIC
+#include "music_cont_in_proxy.h"
+#include "music_cont_out_proxy.h"
+#include "music_event_in_proxy.h"
+#include "music_event_out_proxy.h"
+#include "music_message_in_proxy.h"
+#endif
+
+
 namespace nest
 {
 /**
@@ -53,16 +178,50 @@ public:
   const std::string commandstring( void ) const;
 
 private:
+
   /**
-   * Adds all the known synapse models to the NEST kernel by calling
-   * kernel().model_manager.register_connection_model() with the given
-   * connection data type and name. This is an internal helper function
-   * that is called during construction of the ModelsModule and is not
+   * Registers a particular synapse model with the kernel. The type of the
+   * added synapse is `ConnectionClassT`. The name of the synapse should be
+   * passed as the `name` parameter.
+   *
+   * The synapse model is added three times:
+   * - once for the normal indexing type (TargetIdentifierPtrRport)
+   * - once for the ConnectionLabel-wrapped type (ConnectionLabel<
+   *   ConnectionClassT < TargetIdentifierPtrRport > > >); the name is
+   *   suffixed with "_lbl"
+   * - once for the high-performance indexing type
+   *   (TargetIdentifierType); the name is suffixed with "_hpc"
+   *
+   * This is a helper function for internal use in the ModelsModule. It
+   * is called during construction of the ModelsModule and is not
    * intended to be used outside of this.
   **/
-  template < typename ConnectionT >
-  void register_connection_models( std::string name_postfix = "" );
+/*  template < typename ConnectionClassT >
+  void register_connection_model( const std::string name );*/
+
+
+
 };
+
+
+
+
+
+template < class ConnectionClassT < Q >  >
+void
+register_connection_model( const std::string name )
+{
+  kernel()
+    .model_manager.register_connection_model< ConnectionClassT < TargetIdentifierPtrRport > >(
+      name );
+  kernel()
+    .model_manager.register_connection_model< ConnectionLabel < ConnectionClassT < TargetIdentifierPtrRport > > >(
+      name + "_lbl" );
+  kernel()
+    .model_manager.register_connection_model< ConnectionClassT < TargetIdentifierIndex > >(
+      name + "_hpc" );
+}
+
 
 
 } // namespace
