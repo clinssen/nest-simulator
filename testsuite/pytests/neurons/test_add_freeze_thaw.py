8a0581f101fecf45be099424ecc0bfbf3db008d4
C.A.P. Linssen <charl@turingbirds.com>
2023-04-24 05:41:06 -0700

    fix pycodestyle and move test to new directory

--- testsuite/pytests/neurons/test_add_freeze_thaw.py
+++ testsuite/pytests/neurons/test_add_freeze_thaw.py
@@ -27,7 +27,8 @@ VM_REF = nest.GetDefaults('iaf_psc_alpha', 'V_m')
 def prepare_kernel():
     nest.ResetKernel()
     nest.local_num_threads = 4
-    
+
+
 @pytest.fixture(scope="module")
 def neurons_a(prepare_kernel):
     """
@@ -36,24 +37,29 @@ def neurons_a(prepare_kernel):
 
     return nest.Create('iaf_psc_alpha', n=BLOCKSIZE, params={'I_e': I_E})
 
+
 @pytest.fixture(scope="module")
 def neurons_b(neurons_a):
     return nest.Create('iaf_psc_alpha', n=BLOCKSIZE, params={'I_e': I_E})
 
+
 @pytest.fixture(scope="module")
 def sim_a(neurons_a):
     nest.Simulate(SIMTIME)
+
 
 @pytest.fixture(scope="module")
 def sim_a_b(neurons_a, neurons_b, sim_a):
     (neurons_a + neurons_b).V_m = VM_REF
     nest.Simulate(SIMTIME)
 
+
 @pytest.fixture(scope="module")
 def sim_frozen(neurons_a, neurons_b, sim_a_b):
     (neurons_a + neurons_b).V_m = VM_REF
     neurons_a.frozen = True
     nest.Simulate(SIMTIME)
+
 
 @pytest.fixture(scope="module")
 def sim_thawed(neurons_a, neurons_b, sim_frozen):
@@ -65,11 +71,14 @@ def sim_thawed(neurons_a, neurons_b, sim_frozen):
 def test_vm_changes(neurons_a, sim_a):
     assert all(np.array(neurons_a.V_m) < VM_REF)
 
+
 def test_vm_changes_more_neurons(neurons_a, neurons_b, sim_a_b):
     assert all(np.array((neurons_a + neurons_b).V_m) < VM_REF)
 
+
 def test_vm_frozen(neurons_a, neurons_b, sim_frozen):
     assert all(np.array(neurons_a.V_m) == VM_REF) and all(np.array(neurons_b.V_m) < VM_REF)
+
 
 def test_vm_thawed(neurons_a, neurons_b, sim_thawed):
     assert all(np.array((neurons_a + neurons_b).V_m) < VM_REF)
